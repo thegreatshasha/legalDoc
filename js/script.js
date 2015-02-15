1423959921dc531d7932e33467b1495967ecc496
prasunanand <prasunanand.bitsp@gmail.com>
2015-02-15 23:00:41 +0530

    new

--- js/script.js
+++ js/script.js
@@ -7,6 +7,37 @@ $('#createVariable').click(function(){
 	alert (varName);
 	variables.push(varName);
 	console.log(variables);
+    $('#allfields').append("<li>"+varName+"</li>");
+    $(function() {
+            $("#allfields li").draggable({
+                appendTo: "body",
+                helper: "clone",
+                cursor: "select",
+                revert: "invalid"
+            });
+            initDroppable($("#TextArea1"));
+            function initDroppable($elements) {
+                $elements.droppable({
+                    hoverClass: "textarea",
+                    accept: ":not(.ui-sortable-helper)",
+                    drop: function(event, ui) {
+                        var $this = $(this);
+ 
+                        var tempid = ui.draggable.text();
+                        var dropText;
+                        dropText = " {" + tempid + "} ";
+                        var droparea = document.getElementById('TextArea1');
+                        var range1 = droparea.selectionStart;
+                        var range2 = droparea.selectionEnd;
+                        var val = droparea.value;
+                        var str1 = val.substring(0, range1);
+                        var str3 = val.substring(range1, val.length);
+                        droparea.value = str1 + dropText + str3;
+                    }
+                });
+            }
+        });
+    console.log('hi');
 })
 
 
